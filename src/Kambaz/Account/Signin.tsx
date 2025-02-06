eefc85c758cede4900946465e251c1766e03f8df
likhithreddyrechintala <rlikhith@gmail.com>
2025-02-06 00:56:06 -0500

    changed signin inputs

--- src/Kambaz/Account/Signin.tsx
+++ src/Kambaz/Account/Signin.tsx
@@ -1,17 +1,23 @@
+import { Button, Form, FormControl } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function Signin() {
     return (
         <div id="wd-signin-screen">
             <h3>Sign in</h3>
-            <input className="wd-username" placeholder="username" /> <br />
-            <input className="wd-password" placeholder="password" type="password" /> <br />
-            <Link id="wd-signin-btn" to="/Kambaz/Dashboard">
-                Sign in
-            </Link>
+            <Form.Group className="mb-3" controlId="formAssignmentAvailableUntil">
+                <Form.Control className="wd-username" placeholder="username" />
+                <Form.Control className="wd-password" placeholder="password" type="password" />
+            </Form.Group>
+            <Button id="wd-signin-btn">
+                <Link className="text-decoration-none text-white" to="/Kambaz/Dashboard">
+                    Sign in
+                </Link>
+            </Button>
             <br />
             <Link id="wd-signup-link" to="/Kambaz/Account/Signup">
                 Sign up
             </Link>
+
         </div>
     );
 }
--- src/Kambaz/Courses/Assignments/Editor.tsx
+++ src/Kambaz/Courses/Assignments/Editor.tsx
@@ -1,18 +1,18 @@
-import { Col, Form, Row } from "react-bootstrap";
+import { Button, Col, Form, FormGroup, Row } from "react-bootstrap";
 
 export default function AssignmentEditor() {
     return (
         <Form id="wd-assignments-editor">
             <Form.Group as={Col} controlId="formAssignmentName">
                 <Form.Label>Assignment Name</Form.Label>
-                <Form.Control type="text" placeholder="A1" value="A1" />
+                <Form.Control type="text" className="w-75" placeholder="A1" value="A1" />
             </Form.Group>
             <br />
             <Form.Group className="mb-3" controlId="formAssignmentDescription">
-                <Form.Control as="textarea" rows={10} cols={50} value={`The assignment is available online. \n\nSubmit a link to the landing page ot your Web application running on Netlify. \n\nThe page should include the following: \nYour full name and section Links to each of the lab assignments. \nLink to the Kambaz application. \nLinks to all relevant source code repositories. \n\nThe Kambaz application Should include a link to navigate back to the landing page.`} />
+                <Form.Control as="textarea" className="w-75" rows={10} cols={50} value={`The assignment is available online. \n\nSubmit a link to the landing page ot your Web application running on Netlify. \n\nThe page should include the following: \nYour full name and section Links to each of the lab assignments. \nLink to the Kambaz application. \nLinks to all relevant source code repositories. \n\nThe Kambaz application Should include a link to navigate back to the landing page.`} />
             </Form.Group>
             <Form.Group as={Row} className="mb-3" controlId="formAssignmentPoints">
-                <Form.Label column sm={2}>
+                <Form.Label column sm={2} className="d-flex justify-content-end">
                     Points
                 </Form.Label>
                 <Col sm={10}>
@@ -20,7 +20,7 @@ export default function AssignmentEditor() {
                 </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3" controlId="formAssignmentGroup">
-                <Form.Label column sm={2}>
+                <Form.Label column sm={2} className="d-flex justify-content-end">
                     Assignment Group
                 </Form.Label>
                 <Col sm={10}>
@@ -33,7 +33,7 @@ export default function AssignmentEditor() {
                 </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3" controlId="formAssignmentGradeDisplay">
-                <Form.Label column sm={2}>
+                <Form.Label column sm={2} className="d-flex justify-content-end">
                     Display Grade as
                 </Form.Label>
                 <Col sm={10}>
@@ -44,68 +44,70 @@ export default function AssignmentEditor() {
                 </Col>
             </Form.Group>
             <Form.Group as={Row} className="mb-3" controlId="formAssignmentSubmissionType">
-                <Form.Label column sm={2}>
+                <Form.Label column sm={2} className="d-flex justify-content-end">
                     Submission Type
                 </Form.Label>
-                <Col sm={10}>
-                    <Form.Select className="w-50" >
+                <Col sm={10} className="border border-secondary w-50 rounded-3 p-3">
+                    <Form.Select>
                         <option value="online">Online</option>
                         <option value="offline">Offline</option>
                     </Form.Select>
+                    <Form.Group as={Row} className="my-2" controlId="formAssignmentOnlineEntryOptions">
+                        <Form.Label column className="fw-bold">
+                            Online Entry Options
+                        </Form.Label>
+                        <Form.Check type="checkbox" id="wd-text-entry" className="m-3" label="Text Entry" />
+                        <Form.Check type="checkbox" id="wd-website-url" className="m-3" label="Website URL" />
+                        <Form.Check type="checkbox" id="wd-media-recordings" className="m-3" label="Media Recordings" />
+                        <Form.Check type="checkbox" id="wd-student-annotation" className="m-3" label="Student Annotation" />
+                        <Form.Check type="checkbox" id="wd-file-upload" className="m-3" label="File Uploads" />
+
+                    </Form.Group>
+                </Col>
+            </Form.Group>
+            <Form.Group as={Row} className="mb-3" controlId="formAssignmentAssignTo">
+                <Form.Label column sm={2} className="d-flex justify-content-end">
+                    Assign
+                </Form.Label>
+                <Col sm={10} className="border border-secondary w-50 rounded-3 p-3">
+                    <Form.Group className="mb-3" controlId="formAssignmentAssignTo">
+                        <Form.Label column>
+                            Assign To
+                        </Form.Label>
+                        <Form.Control type="text" value="Everyone" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formAssignmentDueDate">
+                        <Form.Label column>
+                            Due
+                        </Form.Label>
+                        <Form.Control type="date" value="2024-05-13" />
+                    </Form.Group>
+                    <Form.Group as={Row}>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="formAssignmentAvailableFrom">
+                                <Form.Label>
+                                    Available from
+                                </Form.Label>
+                                <Form.Control type="date" value="2024-05-06" />
+                            </Form.Group>
+                        </Col>
+                        <Col>
+                            <Form.Group className="mb-3" controlId="formAssignmentAvailableUntil">
+                                <Form.Label>
+                                    Until
+                                </Form.Label>
+                                <Form.Control type="date" value="2024-05-20" />
+                            </Form.Group>
+                        </Col>
+                    </Form.Group>
                 </Col>
             </Form.Group>
 
-            <table>
-
-                <br />
-                <tr>
-                    <td align="right" valign="top" />
-                    <td>
-                        Online Entry Options<br />
-                        <input type="checkbox" name="online-entry-options" id="wd-text-entry" />
-                        <label htmlFor="wd-text-entry">Text Entry</label><br />
-                        <input type="checkbox" name="online-entry-options" id="wd-website-url" />
-                        <label htmlFor="wd-website-url">Website URL</label><br />
-                        <input type="checkbox" name="online-entry-options" id="wd-media-recordings" />
-                        <label htmlFor="wd-media-recordings">Media Recordings</label><br />
-                        <input type="checkbox" name="online-entry-options" id="wd-student-annotation" />
-                        <label htmlFor="wd-student-annotation">Student Annotation</label><br />
-                        <input type="checkbox" name="online-entry-options" id="wd-file-upload" />
-                        <label htmlFor="wd-file-upload">File Uploads</label><br />
-                    </td>
-                </tr>
-                <br />
-                <tr>
-                    <td align="right" valign="top">
-                        <label htmlFor="wd-assign-to">Assign</label>
-                    </td>
-                    <td>
-                        <label htmlFor="wd-assign-to">Assign to</label><br />
-                        <input id="wd-assign-to" value="Everyone" />
-                    </td>
-                </tr>
-                <br />
-                <tr>
-                    <td align="right" valign="top" />
-                    <td>
-                        <label htmlFor="wd-due-date">Due</label><br />
-                        <input type="date" value="2024-05-13" id="wd-due-date" /><br />
-                    </td>
-                </tr>
-                <br />
-                <tr>
-                    <td align="right" valign="top" />
-                    <td>
-                        <label htmlFor="wd-available-from">Available from</label><br />
-                        <input type="date" value="2024-05-06" id="wd-available-from" /><br />
-                    </td>
-                    <td>
-                        <label htmlFor="wd-available-until">Until</label><br />
-                        <input type="date" value="2024-05-20" id="wd-available-until" /><br />
-                    </td>
-                </tr>
-            </table>
-            <button id="wd-cancel">Cancel</button>&nbsp;&nbsp;<button id="wd-save">Save</button>
+            <br />
+            <div className="d-flex justify-content-end w-75">
+                <Button variant="secondary" className="mx-2" id="wd-cancel">Cancel</Button>
+                <Button variant="danger" className="mx-2" id="wd-save">Save</Button>
+            </div>
         </Form>
     );
 }
