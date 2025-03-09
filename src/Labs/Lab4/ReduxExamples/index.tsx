9f59ed8e8fd56fd8edc4634ecd54a4e416f6c228
Likhith Reddy Rechintala <54302844+likhithreddy@users.noreply.github.com>
2025-03-09 19:16:37 -0400

    done until section 4.3.4

---
+++ src/Labs/Lab4/ReduxExamples/index.tsx
@@ -0,0 +1,11 @@
+import CounterRedux from "./CounterRedux";
+
+export default function ReduxExamples() {
+    return (
+        <div>
+            <h2>Redux Examples</h2>
+            <CounterRedux />
+        </div>
+    );
+};
+
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,20 +1,27 @@
-import Lab1 from "./Lab1";
 import { Route, Routes, Navigate } from "react-router";
+import { Provider } from "react-redux";
+import store from "./store";
 import TOC from "./TOC.tsx";
+import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
+
 export default function Labs() {
     return (
-        <div>
-            <h1>Labs - Likhith Reddy Rechintala - Section 01</h1>
-            <a href="https://github.com/likhithreddy/kambaz-web-application.git" target="_blank" className="fs-4 text-decoration-none" id="wd-github">Kambaz Github Code Repository</a>
-            <TOC />
-            <Routes>
-                <Route path="/" element={<Navigate to="Lab1" />} />
-                <Route path="Lab1" element={<Lab1 />} />
-                <Route path="Lab2/*" element={<Lab2 />} />
-                <Route path="Lab3/*" element={<Lab3 />} />
-            </Routes>
-        </div>
+        <Provider store={store}>
+            <div className="container-fluid">
+                <h1>Labs - Likhith Reddy Rechintala - Section 01</h1>
+                <a href="https://github.com/likhithreddy/kambaz-web-application.git" target="_blank" className="fs-4 text-decoration-none" id="wd-github">Kambaz Github Code Repository</a>
+                <TOC />
+                <Routes>
+                    <Route path="/" element={<Navigate to="Lab1" />} />
+                    <Route path="Lab1" element={<Lab1 />} />
+                    <Route path="Lab2/*" element={<Lab2 />} />
+                    <Route path="Lab3/*" element={<Lab3 />} />
+                    <Route path="Lab4/*" element={<Lab4 />} />
+                </Routes>
+            </div>
+        </Provider>
     );
 }
