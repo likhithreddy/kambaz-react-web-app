5aad58120591a93b8a3a128183d350896eb786d2
Likhith Reddy Rechintala <54302844+likhithreddy@users.noreply.github.com>
2025-03-30 19:10:17 -0400

    implemented until 5.3.3.2

--- src/Kambaz/Account/Profile.tsx
+++ src/Kambaz/Account/Profile.tsx
@@ -3,17 +3,24 @@ import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setCurrentUser } from "./reducer";
+import * as client from "./client";
 
 export default function Profile() {
     const [profile, setProfile] = useState<any>({});
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const { currentUser } = useSelector((state: any) => state.accountReducer);
+    const updateProfile = async () => {
+        const updatedProfile = await client.updateUser(profile);
+        dispatch(setCurrentUser(updatedProfile));
+    };
+
     const fetchProfile = () => {
         if (!currentUser) return navigate("/Kambaz/Account/Signin");
         setProfile(currentUser);
     };
-    const signout = () => {
+    const signout = async () => {
+        await client.signout();
         dispatch(setCurrentUser(null));
         navigate("/Kambaz/Account/Signin");
     };
@@ -25,6 +32,7 @@ export default function Profile() {
             {profile && (
                 <div>
                     <Form>
+                        <button onClick={updateProfile} className="btn btn-primary w-100 mb-2"> Update </button>
                         <Form.Group className="my-2" controlId="formUsername">
                             <Form.Control type="text" id="wd-username" defaultValue={profile.username} placeholder="Username" onChange={(e) => setProfile({ ...profile, username: e.target.value })} />
                         </Form.Group>
@@ -56,6 +64,7 @@ export default function Profile() {
                         </Button>
                     </Form>
                 </div>
+
             )}
         </div>
     );
--- src/Kambaz/Account/Signup.tsx
+++ src/Kambaz/Account/Signup.tsx
@@ -1,18 +1,27 @@
-import { Form, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import * as client from "./client";
+import { useDispatch } from "react-redux";
+import { setCurrentUser } from "./reducer";
+import { FormControl } from "react-bootstrap";
 export default function Signup() {
+    const [user, setUser] = useState<any>({});
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const signup = async () => {
+        const currentUser = await client.signup(user);
+        dispatch(setCurrentUser(currentUser));
+        navigate("/Kambaz/Account/Profile");
+    };
     return (
-        <div id="wd-signup-screen">
-            <h3>Sign up</h3>
-            <Form.Group className="mb-3" controlId="formUserDetails">
-                <Form.Control className="wd-user-credentials my-2" id="wd-username" placeholder="username" />
-                <Form.Control className="wd-user-credentials my-2" id="wd-password" placeholder="password" type="password" />
-                <Form.Control className="wd-user-credentials my-2" id="wd-password-verify" placeholder="verify password" type="password" />
-            </Form.Group>
-            <Button className="w-100" id="wd-signup-btn">
-                <Link className="text-decoration-none text-white" to="/Kambaz/Account/Profile" > Sign up </Link>
-            </Button>
-            <Link className="my-3" to="/Kambaz/Account/Signin" >Sign in</Link>
+        <div className="wd-signup-screen">
+            <h1>Sign up</h1>
+            <FormControl value={user.username} onChange={(e) => setUser({ ...user, username: e.target.value })}
+                className="wd-username b-2" placeholder="username" />
+            <FormControl value={user.password} onChange={(e) => setUser({ ...user, password: e.target.value })}
+                className="wd-password mb-2" placeholder="password" type="password" />
+            <button onClick={signup} className="wd-signup-btn btn btn-primary mb-2 w-100"> Sign up </button><br />
+            <Link to="/Kambaz/Account/Signin" className="wd-signin-link">Sign in</Link>
         </div>
     );
 }
