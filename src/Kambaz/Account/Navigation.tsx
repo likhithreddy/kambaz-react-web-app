9aa65e7ca681c9c003a09c2922f81578cfba49da
Likhith Reddy Rechintala <54302844+likhithreddy@users.noreply.github.com>
2025-04-17 00:38:52 -0400

    implemented until 6.6.2.3

--- src/Kambaz/Account/Navigation.tsx
+++ src/Kambaz/Account/Navigation.tsx
@@ -2,21 +2,32 @@ import { Link, useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function AccountNavigation() {
-    const { currentUser } = useSelector((state: any) => state.accountReducer);
-    const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
-    const { pathname } = useLocation();
+  const { currentUser } = useSelector((state: any) => state.accountReducer);
+  const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+  const { pathname } = useLocation();
+  const active = (path: string) => (pathname.includes(path) ? "active" : "");
 
-    return (
-        <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-            {links.map((link) => (
-                <Link
-                    key={link}
-                    to={`/Kambaz/Account/${link}`}
-                    className={`list-group-item ${pathname === `/Kambaz/Account/${link}` ? 'active' : ''} border border-0`}
-                >
-                    {link}
-                </Link>
-            ))}
-        </div>
-    );
+  return (
+    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
+      {links.map((link) => (
+        <Link
+          key={link}
+          to={`/Kambaz/Account/${link}`}
+          className={`list-group-item border border-0 ${
+            pathname === `/Kambaz/Account/${link}` ? "active" : ""
+          }`}
+        >
+          {link}
+        </Link>
+      ))}
+      {currentUser && currentUser.role === "ADMIN" && (
+        <Link
+          to={`/Kambaz/Account/Users`}
+          className={`list-group-item border border-0 ${active("Users")}`}
+        >
+          Users
+        </Link>
+      )}
+    </div>
+  );
 }
